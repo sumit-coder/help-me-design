c8aac84cb250f9ac1bfa33fa1bebdf5adea36875
sumit-coder <sumitbu24@gmail.com>
2023-05-21 13:03:07 +0530

    tech list on docs tab

--- lib/views/screens/tabs/docs_screen.dart
+++ lib/views/screens/tabs/docs_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_animate/flutter_animate.dart';
 import 'package:help_me_design/theme/my_colors.dart';
 import 'package:help_me_design/theme/my_design_system.dart';
 import 'package:help_me_design/theme/my_theme.dart';
+import 'package:help_me_design/views/widgets/button_tap_effect.dart';
 
 import '../../../constants/text_constants.dart';
 import '../../widgets/container_pattern_painter.dart';
@@ -90,6 +91,23 @@ class DocsView extends StatelessWidget {
               ),
             ),
             Container(
+              // height: 88,
+              padding: const EdgeInsets.all(6),
+              margin: EdgeInsets.only(left: MySpaceSystem.spaceX3, bottom: MySpaceSystem.spaceX3),
+              decoration: BoxDecoration(
+                boxShadow: cardShadow,
+                borderRadius: BorderRadius.circular(8),
+                color: themeData.colorScheme.secondary,
+              ),
+              child: Row(
+                children: [
+                  TechCard(techName: "Saved", icon: Icons.bookmark_added_rounded, onTap: () {}),
+                  TechCard(techName: "Dart", onTap: () {}),
+                  TechCard(techName: "Flutter", onTap: () {}),
+                ],
+              ),
+            ),
+            Container(
               margin: EdgeInsets.only(left: MySpaceSystem.spaceX3),
               // width: 660,
               // height: 300,
@@ -114,6 +132,52 @@ class DocsView extends StatelessWidget {
             )
           ],
         ).animate().scaleXY(begin: 0.1).move(begin: const Offset(-250, 300)).flipH(begin: -0.1),
+      ),
+    );
+  }
+}
+
+class TechCard extends StatelessWidget {
+  const TechCard({
+    super.key,
+    required this.techName,
+    required this.onTap,
+    this.icon,
+  });
+
+  final String techName;
+  final VoidCallback onTap;
+  final IconData? icon;
+
+  @override
+  Widget build(BuildContext context) {
+    var themeData = Theme.of(context);
+    return ButtonTapEffect(
+      onTap: () {},
+      child: Container(
+        // height: 54,
+        margin: const EdgeInsets.only(right: 8),
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        decoration: BoxDecoration(
+          boxShadow: cardShadow,
+          borderRadius: BorderRadius.circular(4),
+          color: themeData.colorScheme.primary,
+        ),
+        child: Row(
+          children: [
+            icon != null
+                ? SizedBox(
+                    child: Icon(
+                      icon,
+                      size: 28,
+                      color: DesignSystemColors.secondaryColorDark,
+                    ),
+                  )
+                : SizedBox(height: 24, child: Image.network('https://upload.wikimedia.org/wikipedia/commons/c/c6/Dart_logo.png')),
+            SizedBox(width: MySpaceSystem.spaceX2),
+            Text(techName, style: MyTextTypeSystem.titleLargeDark),
+          ],
+        ),
       ),
     );
   }
