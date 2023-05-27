b85fbe5a815d444d7d2bc0ea40373f953e529f4c
sumit-coder <sumitbu24@gmail.com>
2023-05-27 17:58:10 +0530

    worked on components tab

--- lib/providers/snippet_tab_provider.dart
+++ lib/providers/snippet_tab_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SnippetTabProvider with ChangeNotifier {
-  bool openActiveSnippetCollectionView = true;
+  bool openActiveSnippetCollectionView = false;
 
   changeCollectionView(bool newValue) {
     openActiveSnippetCollectionView = newValue;
---
+++ lib/views/screens/tabs/components_tab/widgets/code_snippet_list_view.dart
@@ -0,0 +1,139 @@
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/material.dart';
+import 'package:help_me_design/providers/snippet_tab_provider.dart';
+import 'package:help_me_design/theme/my_design_system.dart';
+import 'package:help_me_design/theme/my_theme.dart';
+import 'package:help_me_design/views/widgets/button_tap_effect.dart';
+import 'package:help_me_design/views/widgets/tag_widget.dart';
+import 'package:provider/provider.dart';
+
+class ComponentsListView extends StatelessWidget {
+  ComponentsListView({
+    super.key,
+  });
+
+  final List snippetCollectionList = [
+    "def",
+    "def",
+    "def",
+    "def",
+    "def",
+    "def",
+    "def",
+    "def",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    var themeData = Theme.of(context);
+    var snippetTabProvider = Provider.of<SnippetTabProvider>(context);
+    return Container(
+      margin: EdgeInsets.only(left: MySpaceSystem.spaceX3),
+      // width: 660,
+      // height: 300,
+      child: Wrap(
+        spacing: MySpaceSystem.spaceX3,
+        runSpacing: MySpaceSystem.spaceX3,
+        // direction: Axis.vertical,
+        children: [
+          AddComponentCollectionCard(),
+          for (var snippetCollection in snippetCollectionList)
+            ButtonTapEffect(
+              onTap: () {
+                snippetTabProvider.changeCollectionView(true);
+              },
+              child: Container(
+                height: 154,
+                width: 300,
+                padding: EdgeInsets.all(MySpaceSystem.spaceX2),
+                decoration: BoxDecoration(
+                  color: themeData.colorScheme.secondary,
+                  boxShadow: cardShadow,
+                  borderRadius: const BorderRadius.all(Radius.circular(8)),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: const [
+                        Tag(title: 'Flutter'),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Flutter utility widgets',
+                          maxLines: 2,
+                          style: themeData.textTheme.titleSmall,
+                        ),
+                        SizedBox(height: MySpaceSystem.spaceX2),
+                        Text('20 Snippets', style: themeData.textTheme.bodyMedium),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class AddComponentCollectionCard extends StatelessWidget {
+  const AddComponentCollectionCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var themeData = Theme.of(context);
+    return ButtonTapEffect(
+      onTap: () {},
+      child: Material(
+        elevation: 3,
+        borderRadius: const BorderRadius.all(Radius.circular(8)),
+        child: Container(
+          height: 150,
+          width: 300,
+          // padding: EdgeInsets.all(MySpaceSystem.spaceX2),
+          decoration: BoxDecoration(
+            boxShadow: cardShadow,
+            borderRadius: const BorderRadius.all(Radius.circular(8)),
+            color: themeData.colorScheme.primary,
+            // border: Border.all(width: 1, color: themeData.colorScheme.outline),
+          ),
+          child: DottedBorder(
+            color: themeData.colorScheme.outline,
+            radius: Radius.circular(8),
+            borderType: BorderType.RRect,
+            strokeWidth: 2,
+            dashPattern: const [5, 4],
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.add_rounded,
+                    size: 44,
+                    color: themeData.colorScheme.secondary,
+                  ),
+                  SizedBox(height: MySpaceSystem.spaceX2),
+                  Text(
+                    'Components Collection',
+                    style: themeData.textTheme.bodyMedium!.copyWith(
+                      color: themeData.colorScheme.secondary,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
