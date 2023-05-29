35ca96822fd990810004ba8e1989bb799eca0bc1
sumit-coder <sumitbu24@gmail.com>
2023-05-29 22:16:32 +0530

    some work on explore tab

--- lib/views/screens/tabs/explore_tab/explore_tab.dart
+++ lib/views/screens/tabs/explore_tab/explore_tab.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:help_me_design/theme/my_colors.dart';
@@ -6,6 +5,7 @@ import 'package:help_me_design/theme/my_design_system.dart';
 import 'package:help_me_design/theme/my_theme.dart';
 
 import '../../../widgets/container_pattern_painter.dart';
+import '../widgets/tab_view_hero_card.dart';
 
 class ExploreView extends StatelessWidget {
   ExploreView({Key? key}) : super(key: key);
@@ -24,68 +24,13 @@ class ExploreView extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Container(
-              margin: EdgeInsets.only(left: MySpaceSystem.spaceX3, bottom: MySpaceSystem.spaceX3),
-              decoration: BoxDecoration(
-                boxShadow: cardShadow,
-                borderRadius: BorderRadius.circular(8),
-                color: themeData.colorScheme.secondary,
-              ),
-              // width: 630,
-              height: 250,
-              child: Stack(
-                children: [
-                  CustomPaint(
-                    painter: ContainerPatternPainter(70, context),
-                    child: Container(),
-                  ),
-                  Positioned(
-                    right: 0,
-                    top: 0,
-                    bottom: 0,
-                    child: SizedBox(
-                      // width: 300,
-                      child: ClipRRect(
-                        borderRadius: const BorderRadius.only(bottomRight: Radius.circular(8), topRight: Radius.circular(8)),
-                        child: Image.network(
-                          "https://i.ibb.co/SNVPkKM/original-dd50f8430ab324b03b6af592e73ca6c7-removebg-preview.png",
-                          fit: BoxFit.cover,
-                          alignment: Alignment.topCenter,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    left: MySpaceSystem.spaceX3,
-                    bottom: MySpaceSystem.spaceX3,
-                    child: SizedBox(
-                      width: 300,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            padding: EdgeInsets.all(4),
-                            color: DesignSystemColors.primaryColor,
-                            child: Text(
-                              'Explore.',
-                              style: MyTextTypeSystem.titleXXLargeDark.copyWith(fontSize: 34, height: 1.0),
-                            ),
-                          ),
-                          SizedBox(height: MySpaceSystem.spaceX3),
-                          Container(
-                            padding: EdgeInsets.all(4),
-                            color: DesignSystemColors.primaryColor,
-                            child: Text(
-                              'Welcome to HelpMeDesign, \nHere you can Explore, Add & Create. Design Resources, Design Systems, UI Components etc.',
-                              style: MyTextTypeSystem.titleMediumDark.copyWith(height: 1.3),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+            TabViewHeroCard(
+              // title: MyTextConstants.docsTabHeadline,
+              title: "Explore.",
+              shortDescription:
+                  "Welcome to HelpMeDesign, \nHere you can Explore, Add & Create. Design Resources, Design Systems, UI Components etc.",
+              posterImage: 'https://i.ibb.co/SNVPkKM/original-dd50f8430ab324b03b6af592e73ca6c7-removebg-preview.png',
+              bgPattern: ContainerPatternPainter(70, context),
             ),
             Container(
               margin: EdgeInsets.only(left: MySpaceSystem.spaceX3),
