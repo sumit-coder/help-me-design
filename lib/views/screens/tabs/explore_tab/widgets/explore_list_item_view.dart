bf450b41359919039f4ff915b813cdec1a8c4d44
sumit-coder <sumitbu24@gmail.com>
2023-06-09 18:31:57 +0530

    added card widgets for explore tab

--- lib/views/screens/tabs/explore_tab/widgets/explore_list_item_view.dart
+++ lib/views/screens/tabs/explore_tab/widgets/explore_list_item_view.dart
@@ -7,12 +7,13 @@ import 'package:help_me_design/views/widgets/button_tap_effect.dart';
 import 'package:help_me_design/views/widgets/send_back_bar_with_title.dart';
 import 'package:provider/provider.dart';
 
+import 'cards/explore_resource_card.dart';
+
 class ExploreListItemView extends StatelessWidget {
   const ExploreListItemView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    var themeData = Theme.of(context);
     var exploreTapProvider = Provider.of<ExploreTabProvider>(context);
     var activeItemData = exploreTapProvider.designResourcesCollection!.data[exploreTapProvider.showListItemViewIndex];
     return Container(
@@ -34,70 +35,11 @@ class ExploreListItemView extends StatelessWidget {
             },
           ),
           for (var i = 0; i < activeItemData.resourcesList.length; i++)
-            ButtonTapEffect(
-              onTap: () {
-                // exploreTapProvider.setActiveItemView(i);
-              },
-              child: Container(
-                height: 240,
-                width: 220,
-                padding: EdgeInsets.all(MySpaceSystem.spaceX3),
-                decoration: BoxDecoration(
-                  boxShadow: cardShadow,
-                  borderRadius: BorderRadius.circular(8),
-                  color: themeData.colorScheme.secondary,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // const Icon(Icons.design_services),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          width: 38,
-                          height: 38,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(4),
-                            child: Image.network(
-                              "http://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${activeItemData.resourcesList[i].url}&size=128",
-                              errorBuilder: (context, error, stackTrace) => Icon(Icons.design_services),
-                            ),
-                          ),
-                        ),
-                        ButtonTapEffect(
-                          child: Icon(Icons.open_in_new_rounded, color: themeData.colorScheme.primary),
-                          onTap: () {
-                            UtilityHelper.launchUrlNow(activeItemData.resourcesList[i].url);
-                          },
-                        )
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          activeItemData.resourcesList[i].title,
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                          style: themeData.textTheme.titleMedium,
-                        ),
-                        SizedBox(height: MySpaceSystem.spaceX2),
-                        SizedBox(
-                          child: Text(
-                            activeItemData.resourcesList[i].description,
-                            maxLines: 2,
-                            style: themeData.textTheme.bodyMedium,
-                          ),
-                        ),
-                        SizedBox(height: MySpaceSystem.spaceX1),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
+            ExploreResourceCard(
+              onTap: () {},
+              title: activeItemData.resourcesList[i].title,
+              description: activeItemData.resourcesList[i].title,
+              resourceUrl: activeItemData.resourcesList[i].url,
             ),
         ],
       ),
--- lib/views/screens/tabs/explore_tab/widgets/explore_list_view.dart
+++ lib/views/screens/tabs/explore_tab/widgets/explore_list_view.dart
@@ -6,12 +6,13 @@ import 'package:help_me_design/views/widgets/button_tap_effect.dart';
 import 'package:help_me_design/views/widgets/send_back_bar_with_title.dart';
 import 'package:provider/provider.dart';
 
+import 'cards/explore_resource_collection_card.dart';
+
 class ExploreListView extends StatelessWidget {
   const ExploreListView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    var themeData = Theme.of(context);
     var exploreTapProvider = Provider.of<ExploreTabProvider>(context);
     return Container(
       margin: EdgeInsets.only(left: MySpaceSystem.spaceX3),
@@ -23,62 +24,14 @@ class ExploreListView extends StatelessWidget {
         // direction: Axis.vertical,
         children: [
           for (var i = 0; i < exploreTapProvider.designResourcesCollection!.data.length; i++)
-            ButtonTapEffect(
+            ExploreResourceCollectionCard(
+              description: exploreTapProvider.designResourcesCollection!.data[i].description,
               onTap: () {
                 exploreTapProvider.setActiveItemView(index: i, viewValue: true);
               },
-              child: Container(
-                height: 240,
-                width: 220,
-                padding: EdgeInsets.all(MySpaceSystem.spaceX3),
-                decoration: BoxDecoration(
-                  boxShadow: cardShadow,
-                  borderRadius: BorderRadius.circular(8),
-                  color: themeData.colorScheme.secondary,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // const Icon(Icons.design_services),
-                    SizedBox(
-                      width: 38,
-                      height: 38,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(4),
-                        child: Image.network(
-                          "http://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${exploreTapProvider.designResourcesCollection!.data[i].popularResourceUrl}&size=128",
-                          errorBuilder: (context, error, stackTrace) => Icon(Icons.design_services),
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          exploreTapProvider.designResourcesCollection!.data[i].title,
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 2,
-                          style: themeData.textTheme.titleMedium,
-                        ),
-                        SizedBox(height: MySpaceSystem.spaceX2),
-                        Text(
-                          '${exploreTapProvider.designResourcesCollection!.data[i].description}',
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style: themeData.textTheme.bodyMedium,
-                        ),
-                        SizedBox(height: MySpaceSystem.spaceX2),
-                        Text(
-                          'Count: ${exploreTapProvider.designResourcesCollection!.data[i].resourcesCount}',
-                          style: themeData.textTheme.bodySmall,
-                        ),
-                        SizedBox(height: MySpaceSystem.spaceX1),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
+              popularResourceUrl: exploreTapProvider.designResourcesCollection!.data[i].popularResourceUrl,
+              resourcesCount: exploreTapProvider.designResourcesCollection!.data[i].resourcesCount.toString(),
+              title: exploreTapProvider.designResourcesCollection!.data[i].title,
             ),
         ],
       ),
