ecd929ab21e7cc9b7760c5444208185dbd9506d7
sumit-coder <sumitbu24@gmail.com>
2023-06-14 18:22:22 +0530

    fixed all tabs short description

--- lib/views/screens/tabs/inspiration_tab/inspiration_tab.dart
+++ lib/views/screens/tabs/inspiration_tab/inspiration_tab.dart
@@ -1,5 +1,6 @@
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:help_me_design/constants/text_constants.dart';
 import 'package:help_me_design/theme/my_design_system.dart';
 import 'package:help_me_design/theme/my_theme.dart';
 import 'package:help_me_design/views/screens/tabs/widgets/tab_view_hero_card.dart';
@@ -22,8 +23,9 @@ class InspirationTab extends StatelessWidget {
           children: [
             TabViewHeroCard(
               // title: MyTextConstants.docsTabHeadline,
-              title: "Inspirations.",
-              shortDescription: "Save Your Design Inspirations Here.",
+              title: MyTextConstants.inspirationsTabHeadline,
+              shortDescription: MyTextConstants.inspirationTabShortDescription,
+
               posterImage: "assets/images/insp-poster.png",
               bgPattern: InspirationTabPatternPainter(44, context),
             ),
